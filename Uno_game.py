72a4a8844c223ad1d6892b3b995777db648c6388
Soe Moe Htet Github <mobilesoemoehtet11@gmail.com>
2022-11-12 17:25:47 +0700

    Basic GUI draft

--- Uno_game.py
+++ Uno_game.py
@@ -1,4 +1,28 @@
+from tkinter import *
+import random
+
+class UnoGUI:
+    def __init__(self):
+        window = Tk()
+        window.title("Uno Game")
+
+        self.imageList = []
+        self.imageList.append(PhotoImage(file = "image/3.png"))
+
+        frame = Frame(window)
+        frame.pack()
+        
+
+        self.labelList = []
+        
+        self.labelList.append(Label(frame, image = self.imageList[0], width = 300, height = 900))
+        
+        self.labelList[0].pack(side = LEFT)
+
+        window.mainloop()
+        
+
 def main():
-    print("HI")
+    unogui = UnoGUI()
 
 main()
