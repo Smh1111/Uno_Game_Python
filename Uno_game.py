daa3798e7edb77836857553c8e9b622e1d75e444
Soe Moe Htet Github <mobilesoemoehtet11@gmail.com>
2022-11-13 00:12:12 +0700

    Draft GUI Uno card

--- Uno_game.py
+++ Uno_game.py
@@ -1,26 +1,41 @@
 from tkinter import *
 import random
+from playsound import playsound
 
 class UnoGUI:
     def __init__(self):
         window = Tk()
         window.title("Uno Game")
 
-        self.imageList = []
-        self.imageList.append(PhotoImage(file = "image/3.png"))
-
-        frame = Frame(window)
-        frame.pack()
         
+        width= 1436
+        height= 764
+        window.geometry(f"{width}x{height}")
+        
+        self.imageList = []
+        self.imageList.append(PhotoImage(file = "image/test.png"))
+        self.imageList.append(PhotoImage(file = "image/test.png"))
 
+        
+        print(width, height)
         self.labelList = []
         
-        self.labelList.append(Label(frame, image = self.imageList[0], width = 300, height = 900))
-        
-        self.labelList[0].pack(side = LEFT)
+        self.labelList.append(Button(window, width= 112, height = 174, borderwidth= 0, image = self.imageList[0], command= self.play))
+        self.labelList.append(Button(window, width= 112, height = 174, borderwidth= 0, image = self.imageList[1], command= self.play))
 
+        #self.labelList[0].pack(padx=5, pady=15, side = LEFT)
+        #self.labelList[1].pack(padx=5, pady=20, side = LEFT)
+
+
+        
+        self.labelList[0].place(x=100, y=500)
+        self.labelList[1].place(x=150, y=500)
+        #Label(window, text="first one", bg="#FF0099", fg="white").place(x=75, y=80)
         window.mainloop()
         
+    def play(self):
+        print("hello")
+        playsound('image/click.wav', False)
 
 def main():
     unogui = UnoGUI()
