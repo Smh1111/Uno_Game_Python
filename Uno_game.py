37522b76a017718d164380ef5bbb7a4a240733ec
Soe Moe Htet Github <mobilesoemoehtet11@gmail.com>
2022-11-13 16:55:35 +0700

    created photoshopped images and arranged.

--- Uno_game.py
+++ Uno_game.py
@@ -7,21 +7,20 @@ class UnoGUI:
         window = Tk()
         window.title("Uno Game")
 
-        
+        window.configure(bg="blue")
         width= 1436
         height= 764
         window.geometry(f"{width}x{height}")
         
         self.imageList = []
-        self.imageList.append(PhotoImage(file = "image/test.png"))
-        self.imageList.append(PhotoImage(file = "image/test.png"))
-
+        self.imageList.append(PhotoImage(file = "res/second version/red/red_1.png"))
+        self.imageList.append(PhotoImage(file = "res/second version/red/red_2.png"))
         
         print(width, height)
         self.labelList = []
         
-        self.labelList.append(Button(window, width= 112, height = 174, borderwidth= 0, image = self.imageList[0], command= self.play))
-        self.labelList.append(Button(window, width= 112, height = 174, borderwidth= 0, image = self.imageList[1], command= self.play))
+        self.labelList.append(Button(window, width= 80, height = 120, border= 3, image = self.imageList[0], command= self.play))
+        self.labelList.append(Button(window, width= 80, height = 120, border= 3, image = self.imageList[1], command= self.play))
 
         #self.labelList[0].pack(padx=5, pady=15, side = LEFT)
         #self.labelList[1].pack(padx=5, pady=20, side = LEFT)
@@ -29,13 +28,13 @@ class UnoGUI:
 
         
         self.labelList[0].place(x=100, y=500)
-        self.labelList[1].place(x=150, y=500)
+        self.labelList[1].place(x=300, y=500)
         #Label(window, text="first one", bg="#FF0099", fg="white").place(x=75, y=80)
         window.mainloop()
         
     def play(self):
         print("hello")
-        playsound('image/click.wav', False)
+        playsound('res/sounds/mouse_click_close.wav', False)
 
 def main():
     unogui = UnoGUI()
