edafd87de657de735b163e03dba8889249ca1d7a
Soe Moe Htet Github <mobilesoemoehtet11@gmail.com>
2022-11-19 22:22:58 +0700

    Saved unecessary code blocks in this file (that might be used again)

---
+++ Snippets.py
@@ -0,0 +1,14 @@
+#def click(self):
+#    playsound('res/sounds/mouse_click_close.wav', False)#
+
+
+# Draw pile 
+#def back_click(self):
+#    playsound('res/sounds/mouse_click_close.wav', False)
+#    cardNumber = random.randint(0, 9)
+#    self.player_cards(cardNumber)
+
+        
+# bankPhoto = PhotoImage(file = f"res/second version/Back/back.png")
+# backCard = Button(window, width= 80, height = 120, border= 3, image = bankPhoto, command= self.back_click)
+# backCard.place(x=1300, y=100)
