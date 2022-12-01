944fc82d5255e7a6d910a81d7586c69c55440cf5
Soe Moe Htet Github <mobilesoemoehtet11@gmail.com>
2022-12-02 02:49:50 +0700

    Added 'check the duplicate card' feature

--- Snippets.py
+++ Snippets.py
@@ -12,3 +12,40 @@
 # bankPhoto = PhotoImage(file = f"res/second version/Back/back.png")
 # backCard = Button(window, width= 80, height = 120, border= 3, image = bankPhoto, command= self.back_click)
 # backCard.place(x=1300, y=100)
+
+
+# No duplicate card number
+# if number not in player_numberList:
+#                 player_numberList.append(number)
+#                 player_colorList.append(colorList[color])
+#                 temp = True
+#             else:
+#                 temp = False
+#             
+#             if temp == True:
+#                 i += 1
+#             else:
+#                 continue    
+
+
+# x = 10
+# y = 10
+# #         
+# i = 0
+# list = []
+# temp = False
+# while(i != 7):
+#     number = random.randint(0, 9)
+#     if number not in list:
+#         list.append(number)
+#         temp = True
+#     else:
+#         temp = False
+#     
+#     if temp == True:
+#         self.Card_put(number, x, y)
+#         x += 80
+#         i += 1
+#     else:
+#         continue
+# #         
