451efcf935d58ccb27e4c87c54bd1add5781bb33
Soe Moe Htet Github <mobilesoemoehtet11@gmail.com>
2022-12-14 19:55:36 +0700

    drawcard feature finished

--- Uno_game.py
+++ Uno_game.py
@@ -7,6 +7,12 @@ from tkinter import ttk
 
 colorList = ["red", "blue", "green", "yellow"]
 
+isDrawpile = False
+Player_clicked = False
+draw_Card = 0
+player_Card = 0
+
+player_draw_Card_List = []
 
 window = Tk()
 window.title("Uno Game")
@@ -32,14 +38,12 @@ class UnoGUI:
 
 
         # Draw pile
-        self.Draw_pile_setup()
+        drawpile = drawPile()
+        drawpile.back_Photo()
 
         window.mainloop()
         
    
-    def Draw_pile_setup(self):
-        drawpile = drawPile()
-
 
 class Sounds(ABC):
     def __init__(self):
@@ -49,38 +53,22 @@ class Sounds(ABC):
     def click(self):
         
         playsound('res/sounds/mouse_click_close.wav', False)
-    
-class drawPile(Sounds):
-    def __init__(self):
-        super().__init__()
-        bankPhoto = PhotoImage(file = f"res/second version/Back/back1.png")
-        backCard = Button(window, width= 80, height = 120, border= 3, image = bankPhoto, command= self.click)
-        backCard.place(x=1300, y=100)
-
-        window.mainloop()
-
-    def click(self):
-        super().click()
-    
 
 class Card():
     def __init__(self):
-        pass
-        # self.red_imageList = {}
-        # self.blue_imageList = {}
-        # self.green_imageList = {}
-        # self.yellow_imageList = {}
+        self.red_imageList = {}
+        self.blue_imageList = {}
+        self.green_imageList = {}
+        self.yellow_imageList = {}
 
-        # self.red_labelList = []
-        # self.blue_labelList = []
-        # self.green_labelList = []
-        # self.yellow_labelList = []
+        self.red_labelList = []
+        self.blue_labelList = []
+        self.green_labelList = []
+        self.yellow_labelList = []
 
         
-        
-        #Label(window, text="first one", bg="#FF0099", fg="white").place(x=75, y=80)
-    
-    def Card_images(self):
+
+    def card_images(self):
         numberList = [i for i in range(10)]
         
         try:
@@ -101,8 +89,8 @@ class Card():
             raise FileNotFoundError
     
    
-    def Card_setup(self):
-    
+    def card_buttons(self):
+        self.card_images()
         for i in range(len(self.red_imageList)): # i = key
             
             self.red_labelList.append(Button(window, width= 80, height = 120, border= 3, image = self.red_imageList[i], command= lambda m = f"{str(i)}": [self.click(f"red {m}")]))
@@ -116,15 +104,82 @@ class Card():
         for i in range(len(self.yellow_imageList)):
             self.yellow_labelList.append(Button(window, width= 80, height = 120, border= 3, image = self.yellow_imageList[i], command= lambda m = f"{str(i)}": [self.click(f"yellow {m}"),]))
         
-    def click(self, title):
-        playsound('res/sounds/mouse_click_close.wav', False)
+    
+    def card_setup(self):
+        self.card_buttons()
+        i = 0
         
+        # check_dict = {}
+        # temp = False
 
+        
+        number = random.randint(0, 9)
+        color = random.randint(0,3)
+
+        if color == 0:
+            self.card = self.red_labelList[int(number)]
+            
+        elif color == 1:
+            self.card = self.blue_labelList[int(number)]
+        
+        elif color == 2:
+            self.card = self.green_labelList[int(number)]
+        
+        elif color == 3:
+            self.card = self.yellow_labelList[int(number)]
+ 
+        self.card.place(x = 500, y = 250)
+        print("Cardddddddddddddddddddddddddddd", self.card)
+        return self.card
+    
+        
+    def click(self):
+        playsound('res/sounds/mouse_click_close.wav', False)
+
+start = 0
+class drawPile(Card):
+    def __init__(self):
+        super().__init__()
+        global start
+        if start == 0:
+            self.click()
+            start += 1
+ 
+    def click(self):
+        super().click()
+        global Player_clicked
+        global draw_Card
+        global isDrawpile
+        global player_Card
+
+        if Player_clicked == True:
+            
+            player_Card.destroy()
+            self.__init__()
+            
+            Player_clicked = False
+
+        if isDrawpile == True:
+            draw_Card.destroy()
+            self.__init__()
+
+
+        drawPile_card = self.card_setup()
+        draw_Card = drawPile_card
+        isDrawpile = True
+
+    def back_Photo(self):
+        bankPhoto = PhotoImage(file = f"res/second version/Back/back1.png")
+        backCard = Button(window, width= 80, height = 120, border= 3, image = bankPhoto, command= self.click)
+        backCard.place(x=1300, y=100)
+        window.mainloop()
 
 class dumpPile():
     def __init__(self):
-        self.dumpPile = []
- 
+        
+        self.x = 500
+        self.y = 250
+
 class Player():
     def __init__(self):
         self.x = 10
@@ -145,7 +200,7 @@ class Player():
         self.player_green_labelList = []
         self.player_yellow_labelList = []
 
-        self.player_check_dict = self.player_cards()
+        self.player_check_dict = self.player_cards() # This will be a dictionary.
 
         self.player_card_buttons()
         print(self.player_check_dict)
@@ -174,45 +229,28 @@ class Player():
     def player_card_buttons(self):
         self.player_card_images()
         for i in range(len(self.player_red_imageList)): # i = key
-            
             self.player_red_labelList.append(Button(window, width= 80, height = 120, border= 3, image = self.player_red_imageList[i], command= lambda m = f"{str(i)}": [self.player_click(f"red {m}")]))
-        
-        # print()
-        # print("player_red_labelList = ", self.player_red_labelList)
-        # print()
-        
+
         for i in range(len(self.player_blue_imageList)):
             self.player_blue_labelList.append(Button(window, width= 80, height = 120, border= 3, image = self.player_blue_imageList[i], command= lambda m = f"{str(i)}": [self.player_click(f"blue {m}"),]))
-        # print()
-        # 
-        # print("player_blue_labelList = ", self.player_blue_labelList)
-        # print()
-        
+     
         for i in range(len(self.player_green_imageList)):
             self.player_green_labelList.append(Button(window, width= 80, height = 120, border= 3, image = self.player_green_imageList[i], command= lambda m = f"{str(i)}": [self.player_click(f"green {m}")]))
         
-        # print("player_green_labelList = ", self.player_green_labelList)
-        # print()
-        
         for i in range(len(self.player_yellow_imageList)):
             self.player_yellow_labelList.append(Button(window, width= 80, height = 120, border= 3, image = self.player_yellow_imageList[i], command= lambda m = f"{str(i)}": [self.player_click(f"yellow {m}"),]))
-
-        # print("player_yellow_labelList = ", self.player_yellow_labelList)
-        # print()
         
     def player_card_setup(self, owned_colorList = 0, owned_numberList = 0):
-        
-        
 
         if owned_colorList != 0 and owned_numberList != 0:
-            print("You got heree................")
+            #print("................player_card_setup................")
             
             self.player_colorList = owned_colorList
             self.player_numberList = owned_numberList
 
-        print("============================")
-        print("self.player_colorList:\n", self.player_colorList)
-        print("self.player_numberList:\n", self.player_numberList)
+        #print("============================")
+        #print("self.player_colorList:\n", self.player_colorList)
+        #print("self.player_numberList:\n", self.player_numberList)
 
         i = 0
         while i != len(self.player_colorList):
@@ -259,7 +297,7 @@ class Player():
             if key in check_dict:
                 self.player_colorList.pop()
                 self.player_numberList.pop()
-                print(Exception("Sorry, No duplicates"))
+                #print(Exception("Sorry, No duplicates"))
                 continue
                  
                 
@@ -275,27 +313,27 @@ class Player():
     def player_click(self, title):
         playsound('res/sounds/mouse_click_close.wav', False)
 
-        print("player click: ", title)
+        #print("player click: ", title)
         
         self.player_hit_the_card(title)
         
         del self.player_check_dict[title]
 
         
-        print("=======Remaining:=========")
-        print(self.player_check_dict)
-        print("==========================")
+        #print("=======Remaining:=========")
+        #print(self.player_check_dict)
+        #print("==========================")
 
         self.player_card_Resetup()
 
 
     def player_hit_the_card(self, title):
         
-        print("Player hit ", title)
+        #print("Player hit ", title)
 
         color, number = title.split(" ")
         index = int(number)
-        print(f"Color is {color}, Index number {index} \n")
+        #print(f"Color is {color}, Index number {index} \n")
 
         if color == "red":
             card = self.player_red_labelList[index]
@@ -314,22 +352,44 @@ class Player():
         print()
         try:
             self.previous_card.destroy()
+            #print("Player's previous card on the board was destroyed!")
         except:
              
-            print("hello")
+            print("Player's previous card on the board was not destroyed!")
         try:
 
-            card.place(x = 500, y = 200)
-            
-            # ttk.card.pack()
-            # card.place(x = self._x, y = self._y)
-            # canvas.pack()
-            print("Card is placed \n")
+            card.place(x = 500, y = 250)
+           
+            #print("Card is placed \n")
 
         except:
             raise Exception("This does not happen!")
 
         self.previous_card = card   # Card on the board
+
+        # Checking whether a card is on the board or not
+        global isDrawpile
+        global draw_Card
+        global Player_clicked
+        global player_Card
+
+        if isDrawpile == True:
+            drawpile = drawPile()
+            
+
+            print(f"drawpile {draw_Card} \n\n")
+            try:
+                draw_Card.destroy()
+                draw_Card = 0
+                player_Card = card
+                Player_clicked = True
+            except ValueError:
+                raise Exception("It is not destroyed!")
+            
+            isDrawpile = False
+            
+            #print("There is a card on the board!")
+        
 
     def player_card_Resetup(self):
 
@@ -341,15 +401,15 @@ class Player():
             remaining_colorList.append(color)
             remaining_numberList.append(number)
 
-        print("=======Remaining:=========")
-        print(remaining_colorList, remaining_numberList)
-        print("==========================")
+        #print("=======Remaining:=========")
+        #print(remaining_colorList, remaining_numberList)
+        #print("==========================")
         self.x = 10
         self.y = 570
         self.player_card_setup(remaining_colorList, remaining_numberList)
         
 
-class AI(Card):
+class AI():
     def __init__(self):
         
         self.AI_numberList = []
@@ -455,7 +515,7 @@ class AI(Card):
             if key in check_dict:
                 self.AI_colorList.pop()
                 self.AI_numberList.pop()
-                print(Exception("Sorry, No duplicates"))
+                #print(Exception("Sorry, No duplicates"))
                 continue
                  
                 
@@ -463,8 +523,8 @@ class AI(Card):
 
             i += 1
         
-        print("AI_numberList : ", self.AI_numberList)
-        print("AI_colorList : ", self.AI_colorList)
+        #print("AI_numberList : ", self.AI_numberList)
+        #print("AI_colorList : ", self.AI_colorList)
         
         return check_dict
         
@@ -472,7 +532,7 @@ class AI(Card):
    
     def AI_click(self, title):
         playsound('res/sounds/mouse_click_close.wav', False)
-        print("AI click: ", title)
+        #print("AI click: ", title)
         
 
 # class game_logic:
